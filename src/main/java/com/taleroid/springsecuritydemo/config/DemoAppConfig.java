d2a3ef721e569744e1fb8f877be292a5b37316f1
Talwinder Singh <nijjar.login@gmail.com>
2019-10-11 12:01:21 -0400

    added spring mvc setup java based

---
+++ src/main/java/com/taleroid/springsecuritydemo/config/DemoAppConfig.java
@@ -0,0 +1,22 @@
+package com.taleroid.springsecuritydemo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.ViewResolver;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan("com.taleroid.springsecuritydemo")
+public class DemoAppConfig  implements WebMvcConfigurer{
+	@Bean
+	public ViewResolver viewResolver(){
+		 InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		 viewResolver.setPrefix("/WEB-INF/views/");
+		 viewResolver.setSuffix(".jsp");
+		 return viewResolver;
+     }
+}
